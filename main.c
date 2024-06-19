a57ae855e3eadbfe46de348f779370625e15f070
Kakadozo <felipecardozo477@gmail.com>
2024-06-19 16:00:06 -0300

    First commit

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char characterName [] = "Bolsonaro51";
+    int characterAge = 17;
+    printf("O meu nome é %s\n", characterName);
+    printf("%d %d\n", characterAge, characterAge);
+
+    return 0;
+}
