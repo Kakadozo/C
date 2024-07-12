328b838a7b732fe293762910e645fcf3f595e840
Kakadozo <felipecardozo477@gmail.com>
2024-07-12 09:33:15 -0300

    changes

--- main.c
+++ main.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main()
-{
-    char characterName [] = "Bolsonaro51";
-    int characterAge = 17;
-    printf("O meu nome é %s\n", characterName);
-    printf("%d %d\n", characterAge, characterAge);
-
+{    
+    float altura;
+    char genero;
+    printf("Insira seu gênero. Digite H para homem ou M para mulher\n");
+    scanf("%c", &genero);
+    printf("Insira a sua altura\n");
+    scanf("%f", &altura);
+    float resultado;
+    if (genero = 'h')
+    {
+        resultado = (72.7*altura)-58;
+    }
+    else if (genero = 'm')
+    {
+        resultado = (62.1*altura)-44.7;
+    }
+    printf("O seu peso ideal é %f.\n", resultado);
     return 0;
 }
